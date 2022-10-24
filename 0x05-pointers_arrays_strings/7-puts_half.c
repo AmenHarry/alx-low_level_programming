ca41403b6bafded5d85e57b33fd60da17d220556
AmenHarry <98628305+AmenHarry@users.noreply.github.com>
2022-10-25 00:55:34 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,23 +1,30 @@
 #include "main.h"
-#include "2-strlen.c"
 
 /**
- * puts_half - puts half of int
+ *puts_half - prints every other character of a string
+ *@str: A pointer to an int that will be changed
  *
- * @str: string to split
+ *Return: void which means our answer is correct
  */
-void puts_half(char *str)
-{
-	int i;
-	int oE = 0;
 
-	if (_strlen(str) % 2 != 0)
-	{
-		oE += 1;
-	}
-	for (i = (_strlen(str) + oE) / 2; i < _strlen(str); i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+void puts_half(char *str)
+
+{
+int i, last;
+
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar (str[i]);
+}
+
+_putchar ('\n');
+
 }
